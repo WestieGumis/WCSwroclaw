94ab6bc86519c758212302e560769988ef657645
WestieGumis <damian.gruszczynski@gmail.com>
2022-05-30 12:41:18 +0200

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+console.log("działa");
+const header = document.querySelector(".nav");
+
+window.addEventListener("scroll", () => {
+  const scrollPos = window.scrollY;
+  if (scrollPos > 10) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
